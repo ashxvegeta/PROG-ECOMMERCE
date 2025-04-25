a934ed3ba74c4e4901d8b99c1d61587d08b5802c
Ashutosh <ashutoshroy453@gmail.com>
2025-04-24 21:45:08 -0400

    set code to inde blade

--- app/Http/Controllers/Front/FrontController.php
+++ app/Http/Controllers/Front/FrontController.php
@@ -6,11 +6,15 @@ use App\Http\Controllers\Controller; // ✅ ADD THIS
 use Illuminate\Http\Request;
 use App\Models\Admin;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB;
 
 class FrontController extends Controller
 {
     public function index(Request $request)
-    {
-        return view('front.index');
+    { 
+        $result['home_category'] = DB::table('categories')
+        ->where(['status' => 1])
+        ->get();
+        return view('front.index',$result);
     }
 }
