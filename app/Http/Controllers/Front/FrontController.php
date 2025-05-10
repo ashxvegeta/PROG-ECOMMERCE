b5e9a293017e7b0e464855a83eaf297847eea330
Ashutosh <ashutoshroy453@gmail.com>
2025-05-10 12:01:11 -0400

    get featured discounted tranding product

--- app/Http/Controllers/Front/FrontController.php
+++ app/Http/Controllers/Front/FrontController.php
@@ -32,7 +32,66 @@ class FrontController extends Controller
                         ->get(); 
             }
         }
-       
+
+        $result['home_brand'] = DB::table('brands')
+        ->where(['is_home' => 1])
+        ->where(['status' => 1])
+        ->get();
+
+
+        // for gettting  featured data
+        $result['home_featured_product'][$list->id] = 
+        DB::table('products')
+        ->where(['status'=>1])
+        ->where(['is_featured'=>1])
+        ->get();
+        
+        foreach($result['home_featured_product'][$list->id] as $list1){
+                    $result['home_featured_product_attr'][$list1->id] = 
+                    DB::table('products_attr')
+                    ->leftjoin('sizes','sizes.id','=','products_attr.size_id')
+                    ->leftjoin('colors','colors.id','=','products_attr.color_id')
+                    ->where('products_attr.products_id',$list1->id)
+                    ->get();
+
+                   
+        }
+
+        // for gettting  tranding data
+        $result['home_tranding_product'][$list->id] = 
+        DB::table('products')
+        ->where(['status'=>1])
+        ->where(['is_tranding'=>1])
+        ->get();
+        
+        foreach($result['home_tranding_product'][$list->id] as $list1){
+                    $result['home_tranding_product_attr'][$list1->id] = 
+                    DB::table('products_attr')
+                    ->leftjoin('sizes','sizes.id','=','products_attr.size_id')
+                    ->leftjoin('colors','colors.id','=','products_attr.color_id')
+                    ->where('products_attr.products_id',$list1->id)
+                    ->get();
+
+                   
+        }
+   
+        //  gettin dicounted products
+        $result['home_discounted_product'][$list->id] = 
+        DB::table('products')
+        ->where(['status'=>1])
+        ->where(['is_discounted'=>1])
+        ->get();
+        
+        foreach($result['home_discounted_product'][$list->id] as $list1){
+                    $result['home_discounted_product_attr'][$list1->id] = 
+                    DB::table('products_attr')
+                    ->leftjoin('sizes','sizes.id','=','products_attr.size_id')
+                    ->leftjoin('colors','colors.id','=','products_attr.color_id')
+                    ->where('products_attr.products_id',$list1->id)
+                    ->get();
+
+                   
+        }
         return view('front.index',$result);
     }
 }
--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -286,20 +286,15 @@
             <div class="aa-popular-category-area">
               <!-- start prduct navigation -->
              <ul class="nav nav-tabs aa-products-tab">
-                <li class="active"><a href="#popular" data-toggle="tab">Popular</a></li>
-                <li><a href="#featured" data-toggle="tab">Featured</a></li>
-                <li><a href="#latest" data-toggle="tab">Latest</a></li>                    
+                <li class="active"><a href="#featured" data-toggle="tab">Featured</a></li>
+                <li><a href="#tranding" data-toggle="tab">Tranding</a></li>
+                <li><a href="#discounted" data-toggle="tab">discounted</a></li>                    
               </ul>
               <!-- Tab panes -->
               <div class="tab-content">
-                <!-- Start men popular category -->
-                <div class="tab-pane fade in active" id="popular">
-                  <ul class="aa-product-catg aa-popular-slider">
-                    <!-- start single product item -->
-                   
-                    <!-- start single product item -->
-     
-                    <!-- start single product item -->
+                <!-- Start men featured  category -->
+                <div class="tab-pane fade in active" id="featured">
+                  <ul class="aa-product-catg aa-featured-slider">
                     <li>
                       <figure>
                       <a class="aa-product-img" href="#"><img srcc="{{asset('front_assets/img/women/girl-1.png')}}" alt="polo shirt img"></a>
@@ -309,21 +304,28 @@
                           <span class="aa-product-price">$45.50</span><span class="aa-product-price"><del>$65.50</del></span>
                         </figcaption>
                       </figure>                     
-                      
-                      <!-- product badge -->
-                      <!-- <span class="aa-badge aa-sale" href="#">SALE!</span> -->
                     </li>                                                                                   
                   </ul>
                   <a class="aa-browse-btn" href="#">Browse all Product <span class="fa fa-long-arrow-right"></span></a>
+                </div>      
+                <!-- start tranding product category -->
+                <div class="tab-pane fade" id="tranding">
+                 <ul class="aa-product-catg aa-tranding-slider">
+                    <li>
+                      <figure>
+                        <a class="aa-product-img" href="#"><img src="img/women/girl-2.png" alt="polo shirt img"></a>
+                        <a class="aa-add-card-btn"href="#"><span class="fa fa-shopping-cart"></span>Add To Cart</a>
+                        <figcaption>
+                          <h4 class="aa-product-title"><a href="#">Lorem ipsum doller</a></h4>
+                          <span class="aa-product-price">$45.50</span>
+                        </figcaption>
+                      </figure>                           
+                    </li>                                                                                                 
+                  </ul>
                 </div>
-                <!-- / popular product category -->
-                
-                <!-- start featured product category -->
-                <div class="tab-pane fade" id="featured">
-                 <ul class="aa-product-catg aa-featured-slider">
-                    <!-- start single product item -->
-              
-                     <!-- start single product item -->
+          
+                <div class="tab-pane fade" id="discounted">
+                  <ul class="aa-product-catg aa-discounted-slider">
                     <li>
                       <figure>
                         <a class="aa-product-img" href="#"><img src="img/women/girl-2.png" alt="polo shirt img"></a>
@@ -333,60 +335,13 @@
                           <span class="aa-product-price">$45.50</span>
                         </figcaption>
                       </figure>                      
+                      <!-- product badge -->
                       
-                      <!-- product badge -->
-                       <span class="aa-badge aa-sold-out" href="#">Sold Out!</span>
                     </li>
-                    <!-- start single product item -->
+          
+                                                                                                     
+                  </ul>
                  
-                    <!-- start single product item -->
-                  
-                    <!-- start single product item -->
-                
-                    <!-- start single product item -->
-                    
-                    <!-- start single product item -->
-                   
-                    <!-- start single product item -->
-                                                                                                     
-                  </ul>
-                  <a class="aa-browse-btn" href="#">Browse all Product <span class="fa fa-long-arrow-right"></span></a>
-                </div>
-                <!-- / featured product category -->
-
-                <!-- start latest product category -->
-                <div class="tab-pane fade" id="latest">
-                  <ul class="aa-product-catg aa-latest-slider">
-                    <!-- start single product item -->
-                
-                     <!-- start single product item -->
-                    <li>
-                      <figure>
-                        <a class="aa-product-img" href="#"><img src="img/women/girl-2.png" alt="polo shirt img"></a>
-                        <a class="aa-add-card-btn"href="#"><span class="fa fa-shopping-cart"></span>Add To Cart</a>
-                        <figcaption>
-                          <h4 class="aa-product-title"><a href="#">Lorem ipsum doller</a></h4>
-                          <span class="aa-product-price">$45.50</span>
-                        </figcaption>
-                      </figure>                      
-                     
-                      <!-- product badge -->
-                       <span class="aa-badge aa-sold-out" href="#">Sold Out!</span>
-                    </li>
-                    <!-- start single product item -->
-                    
-                    <!-- start single product item -->
-                    
-                    <!-- start single product item -->
-       
-                    <!-- start single product item -->
-                    
-                    <!-- start single product item -->
-              
-                    <!-- start single product item -->
-                                                                                                     
-                  </ul>
-                   <a class="aa-browse-btn" href="#">Browse all Product <span class="fa fa-long-arrow-right"></span></a>
                 </div>
                 <!-- / latest product category -->              
               </div>
@@ -568,8 +523,10 @@
           <div class="aa-client-brand-area">
             <ul class="aa-client-brand-slider">
             <!-- <li><a href="#"><img src="{{asset('front_assets/img/client-brand-wordpress.png')}}" alt="wordPress img"></a></li> -->
-            <li><a href="#"><img src="{{asset('frontend_assets/img/client-brand-wordpress.png')}}" alt="wordPress img"></a></li>
            
+            @foreach($home_brand as $list)
+            <li><a href="#"><img src="{{asset('storage/media/brand/'.$list->image)}}" alt="{{$list->name}}"></a></li>
+           @endforeach
             </ul>
           </div>
         </div>
